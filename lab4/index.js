bf6b496476382c599a55ef5aa505894ab57d7f1e
DariiaIvanova23 bc2024-4 <dashaaivnovaa13@gmail.com>
2024-10-22 22:26:06 +0300

    part 2 commit

--- lab4/index.js
+++ lab4/index.js
@@ -18,3 +18,58 @@ const server = http.createServer();
 server.listen(port, host, () => {
   console.log(`Server adress is http://${host}:${port}`);
 });
+
+
+
+const fsPromises = fs.promises;
+const superagent = require('superagent');
+
+function getCacheFilePath(code) {
+  return path.join(cache, `${code}.jpg`);
+}
+
+
+server.on('request', async (req, res) => {
+  const { method, url } = req;
+  const code = url.slice(1);
+
+  if (method === 'GET') {
+    try {
+      const filePath = getCacheFilePath(code);
+      const data = await fsPromises.readFile(filePath);
+      res.writeHead(200, { 'Content-Type': 'image/jpeg' });
+      res.end(data);
+    } catch (error) {
+      res.writeHead(404, { 'Content-Type': 'text/plain' });
+      res.end('Картинку не знайдено в кеші');
+    }
+  } else if (method === 'PUT') {
+    let body = [];
+    req.on('data', chunk => body.push(chunk));
+    req.on('end', async () => {
+      try {
+        const filePath = getCacheFilePath(code);
+        await fsPromises.writeFile(filePath, Buffer.concat(body));
+        res.writeHead(201, { 'Content-Type': 'text/plain' });
+        res.end('Картинка успішно збережена в кеші');
+      } catch (error) {
+        res.writeHead(500, { 'Content-Type': 'text/plain' });
+        res.end('Помилка збереження картинки');
+      }
+    });
+  } else if (method === 'DELETE') {
+    try {
+      const filePath = getCacheFilePath(code);
+      await fsPromises.unlink(filePath);
+      res.writeHead(200, { 'Content-Type': 'text/plain' });
+      res.end('Картинка успішно видалена');
+    } catch (error) {
+      res.writeHead(404, { 'Content-Type': 'text/plain' });
+      res.end('Картинку не знайдено');
+    }
+  } else {
+
+    res.writeHead(405, { 'Content-Type': 'text/plain' });
+    res.end('Метод не дозволений');
+  }
+});
